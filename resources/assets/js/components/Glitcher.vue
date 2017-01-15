81a44fc17623617ab65d63a48424654bf2a24fad
dankuck <dankuck@gmail.com>
2017-01-14 23:33:14 -0500

    Those are some nice mutators

--- resources/assets/js/components/Glitcher.vue
+++ resources/assets/js/components/Glitcher.vue
@@ -20,6 +20,19 @@ export default {
                   var y = parseInt(Math.random() * data.length);
                   return data.substr(0, x) + data[y] + data.substr(x + 1, y - x) + data[x] + data.substr(y + 1);
                 },
+                function (data) {
+                  var i = parseInt(Math.random() * data.length);
+                  return data.substr(0, i) + "\0" + data.substr(i + 1);
+                },
+                function (data) {
+                  var i = parseInt(Math.random() * data.length);
+                  return data.substr(0, i) + String.fromCharCode(Math.random() * 256) + data.substr(i + 1);
+                },
+                function (data) {
+                  var i = parseInt(Math.random() * data.length);
+                  var length = Math.random() * (data.length - i);
+                  return data.substr(0, i) + data.substr(i, length).split("").reverse().join("") + data.substr(i + length);
+                },
             ],
         };
     },
